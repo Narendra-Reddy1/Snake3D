db200e495aee2de55cb6c4cc44a0173f13012fd4
Narendra-Reddy <58972366+Narendra-Reddy1@users.noreply.github.com>
2023-02-11 22:51:14 +0530

    Added PhotonNetworkWrapper and Scene architecture

---
+++ Assets/_Game/_Scripts/Photon/PhotonNetworkWrapper.cs
@@ -0,0 +1,21 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonNetworkWrapper : MonoBehaviourPunCallbacks
+{
+    #region Variables
+    #endregion Variables
+
+    #region Unity Methods
+
+    #endregion Unity Methods
+
+    #region Photon Callbacks
+   
+    #endregion
+
+    #region Callbacks
+
+    #endregion Callbacks
+}
