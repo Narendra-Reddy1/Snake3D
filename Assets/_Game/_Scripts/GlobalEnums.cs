db0b649bcf7259d5dff3b28db30dcdcce2509d62
Narendra-Reddy <58972366+Narendra-Reddy1@users.noreply.github.com>
2023-02-11 15:00:58 +0530

    Dummy level setup
    Added a dummy snake and level with cubes

---
+++ Assets/_Game/_Scripts/GlobalEnums.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalEnums : MonoBehaviour
+{
+#region Variables
+#endregion Variables
+
+#region Unity Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+#endregion Unity Methods
+
+#region Public Methods
+
+#endregion Public Methods
+
+#region Private Methods
+
+#endregion Private Methods
+
+#region Callbacks
+
+#endregion Callbacks
+}
